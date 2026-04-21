94fecd1f08a534224f292bdf30dcfd54efdf343f
Al Ameen S <alameensameen77@gmail.com>
2026-04-21 23:13:35 +0530

    chore : added essential dependencies and routings

--- src/app.ts
+++ src/app.ts
@@ -1,9 +1,12 @@
 import express from "express"
 import dotenv from "dotenv"
+import userRoutes from "./routes/user.routes"
 
 dotenv.config()
 
 const app = express()
+
+app.use("/user-service" , userRoutes)
 
 app.get("/" , (req , res)=>{
     res.send("Api gateway is running...")
---
+++ src/utils/constants.ts
@@ -0,0 +1,2 @@
+
+export const USER_SERVICE_URL : string = "http://localhost:5001"
